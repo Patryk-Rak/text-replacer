5b893d2ea771fa24e16a1d2cc94ce84d16fccd18
unknown <dan.timefly@gmail.com>
2021-08-27 14:54:16 +0200

    added second part of generated string in txt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
 
-mylines = []                             # Declare an empty list named mylines.
+mylines = []  # Declare an empty list named mylines.
+mylines2 = [] # Declare an empty list named mylines2.
+
 
 
 with open ('Language2.txt', 'rt', encoding="utf16") as myfile:  # Open lorem.txt for reading
@@ -12,13 +14,21 @@ with open ('Language2.txt', 'rt', encoding="utf16") as myfile:  # Open lorem.txt
             new_line = txtline
         else:
             old_line = txtline
-            new_line = old_line.split('=')[1].lstrip()
-
-
-        # old_line = txtline
-        # new_line = old_line.split('=')[0].lstrip()
+            new_line = old_line.split('=')[1]
         mylines.append(new_line)  # add its contents to mylines.
-print(mylines)  # Print the list.      # and print the string.
+    print(mylines)  # Print the list.      # and print the string.
+
+with open('Language2.txt', 'rt', encoding="utf16") as myfile2:  # Open lorem.txt for reading
+    for txtline2 in myfile2:  # For each line, stored as myline,
+
+        regex = re.compile('=')
+        if (regex.search(txtline2) == None):
+            new_line2 = txtline2
+        else:
+            old_line2 = txtline2
+            new_line2 = old_line2.split('=')[0]+'='+'\n'
+        mylines2.append(new_line2)  # add its contents to mylines.
+print(mylines2)  # Print the list.      # and print the string.
 
 with open('Language3.txt', 'w', encoding="utf16") as f:
     for line in mylines:
@@ -26,3 +36,10 @@ with open('Language3.txt', 'w', encoding="utf16") as f:
             f.write("\n")
         else:
             f.write(line)
+
+with open('Language4.txt', 'w', encoding="utf16") as f:
+    for line2 in mylines2:
+        if line2 == "":
+            f.write("\n")
+        else:
+            f.write(line2)
