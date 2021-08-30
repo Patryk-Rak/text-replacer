08b35ba190341c30bfbbb6c44ef3e790e8f971e1
Patryk Rak <dan.timefly@gmail.com>
2021-08-31 00:29:48 +0200

    updated txt info

--- main.py
+++ main.py
@@ -5,44 +5,46 @@ mylines2 = [] # Declare an empty list named mylines2.
 
 
 
-with open ('Language2.txt', 'rt', encoding="utf16") as myfile:  # Open lorem.txt for reading
-    for txtline in myfile:  # For each line, stored as myline,
-
-
-        regex = re.compile('=')
-        if (regex.search(txtline) == None):
-            new_line = txtline
-        else:
-            old_line = txtline
-            new_line = old_line.split("=", 1)[1]
-        mylines.append(new_line)  # add its contents to mylines.
-
-with open('Language2.txt', 'rt', encoding="utf16") as myfile2:  # Open lorem.txt for reading
-    for txtline2 in myfile2:  # For each line, stored as myline,
-
-        regex = re.compile('=')
-        if (regex.search(txtline2) == None):
-            new_line2 = txtline2
-            mylines2.append("")
-        else:
-            old_line2 = txtline2
-            new_line2 = old_line2.split('=')[0]+'='
-            mylines2.append(new_line2 + '\n')  # add its contents to mylines.
-
-
-with open('Language3.txt', 'w', encoding="utf16") as f:
-    for line in mylines:
-        if line == "":
-            f.write("\n")
-        else:
-            f.write(line)
-
-with open('Language4.txt', 'w', encoding="utf16") as f:
-    for line2 in mylines2:
-        if line2 == "":
-            f.write("\n")
-        else:
-            f.write(line2)
+# with open ('Language2.txt', 'rt', encoding="utf16") as myfile:  # Open lorem.txt for reading
+#     for txtline in myfile:  # For each line, stored as myline,
+#
+#
+#         regex = re.compile('=')
+#         if (regex.search(txtline) == None):
+#             new_line = txtline
+#             mylines.append("")
+#         else:
+#             old_line = txtline
+#             new_line = old_line.split("=", 1)[1]
+#             mylines.append(new_line)  # add its contents to mylines.
+#
+#
+# with open('Language2.txt', 'rt', encoding="utf16") as myfile2:  # Open lorem.txt for reading
+#     for txtline2 in myfile2:  # For each line, stored as myline,
+#
+#         regex = re.compile('=')
+#         if (regex.search(txtline2) == None):
+#             new_line2 = txtline2
+#             mylines2.append("")
+#         else:
+#             old_line2 = txtline2
+#             new_line2 = old_line2.split('=')[0]+'='
+#             mylines2.append(new_line2 + '\n')  # add its contents to mylines.
+#
+#
+# with open('Language3.txt', 'w', encoding="utf16") as f:
+#     for line in mylines:
+#         if line == "":
+#             f.write("\n")
+#         else:
+#             f.write(line)
+#
+# with open('Language4.txt', 'w', encoding="utf16") as f:
+#     for line2 in mylines2:
+#         if line2 == "":
+#             f.write("\n")
+#         else:
+#             f.write(line2)
 
 
 
